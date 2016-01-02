7857cc7152bfa2af1f19092180e3ecb517b8d36c
Jeremy Perez <glyde06@hotmail.com>
2016-01-02 17:52:56 -0600

    added a fetch method for jobs

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,10 @@
 class Job < ActiveRecord::Base
+  include HTTParty
+
+  def fetch
+    response = HTTParty.get("#{self.source}")
+    # Example below if you wanted to query the response for something.
+    # response.select {|resp| resp["id"] < 35 }
+    return JSON.parse(response.body)
+  end
 end
