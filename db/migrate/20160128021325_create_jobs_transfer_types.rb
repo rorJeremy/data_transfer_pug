543a2cbf19e79b4b8f4acfecb89b6c9da821eb81
Jeremy Perez <glyde06@hotmail.com>
2016-02-16 20:30:28 -0600

    remove habtm migration because that probably wasn't the way to go.

--- db/migrate/20160128021325_create_jobs_transfer_types.rb
+++
@@ -1,8 +0,0 @@
-class CreateJobsTransferTypes < ActiveRecord::Migration
-  def change
-    create_table :jobs_transfer_types, id: false do |t|
-      t.references :transfer_type, index: true, foreign_key: true
-      t.references :job, index: true, foreign_key: true
-    end
-  end
-end
